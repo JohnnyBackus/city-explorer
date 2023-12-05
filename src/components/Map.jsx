9beb1999cfc41b77cf9ef9fc0befea05824f5a8d
JohnnyBackus <john.p.backus@gmail.com>
2023-12-04 20:46:53 -0800

    it should work but token not recognized

---
+++ src/components/Map.jsx
@@ -0,0 +1,14 @@
+import {If,Then,Else,When} from 'react-if';
+const API_KEY = import.meta.env.VITE_LOCATION_API_KEY;
+
+function Map(props) {
+  return (
+    <When condition={props.lat && props.long}>
+      <figure>
+        <img src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${props.latitude},${props.longitude}&size=400x300&format=png`} width="500" />
+      </figure>
+    </When>
+  )
+}
+
+export default Map;
