c187854b2a38a15fca56a638b343fe3bab775c4f
JohnnyBackus <john.p.backus@gmail.com>
2023-12-05 16:55:55 -0800

    changes made, trying to get render to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import axios from 'axios';
 import LocationForm from './components/Location';
 import Map from './components/Map';
 import ErrorAlert from './components/ErrorAlert';
-const API_KEY = import.meta.env.VITE_LOCATION_API_KEY;
+const LOCATION_API_KEY = import.meta.env.VITE_LOCATION_API_KEY;
+const WEATHER_API = import.meta.env.VITE_WEATHER_API_URL;
 
 
 function App() {
@@ -13,13 +14,14 @@ function App() {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [errorMessage, setErrorMessage] = useState('');
+  const [weatherData, setWeatherData] = useState([]);
   
   function handleExploreCity(cityInput) {
     getLocation(cityInput);
   }
 
   async function getLocation(cityName){
-    let url = `https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${cityName}&format=json`;
+    let url = `https://us1.locationiq.com/v1/search?key=${LOCATION_API_KEY}&q=${cityName}&format=json`;
     console.log(url);
     try {
       let response = await axios.get(url);
@@ -32,9 +34,29 @@ function App() {
     }
   }
 
+  async function getWeatherData(type) {
+    try {
+      let response = await axios.get(`${WEATHER_API}/weather?type=${type}`);
+      console.log(response);
+      setWeatherData(response.data);
+    } catch(error) {
+      console.error(error.message);
+    }
+  }
+
+  function getLat() {
+    // ?type=food
+    getWeatherData("food");
+  }
+
+  function getLong() {
+    // type=supplies
+    getWeatherData("supplies");
+  }
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>City Explorer</h1>
       <div>
         <LocationForm city={city} handleExploreCity={handleExploreCity} lat={latitude} long={longitude} />
         <Map lat={latitude} long={longitude} />
