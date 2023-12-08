4959b618a21ef618d7552312b3039c9abeb035a9
JohnnyBackus <john.p.backus@gmail.com>
2023-12-07 17:36:58 -0800

    added wx icons and partially fixed a bug in movie search by restricting search to only city name and not include state if a comma is used

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -20,29 +20,29 @@ function Weather(props) {
             <h3> Three Day Forecast for {city} </h3> 
             <Row>
                 <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/300px300" />
+                    <Card.Img variant="top" src={`src/assets/icons/${forecastIcon[0]}.png`} />
                     <Card.Body>
                         <Card.Title>Forecast for Today</Card.Title>
                         <Card.Text>
-                            Today will be {forecastDescription[1]} with a high of {forecastMaxTemp[1]} and a low of {forecastLowTemp[1]}.
+                            Today will be {forecastDescription[0]} with a high of {forecastMaxTemp[0]} and a low of {forecastLowTemp[0]}.
                         </Card.Text>
                     </Card.Body>
                 </Card>
                 <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={`src/assets/icons${forecastIcon[2]}`} />
+                    <Card.Img variant="top" src={`src/assets/icons/${forecastIcon[1]}.png`} />
+                    <Card.Body>
+                    <Card.Title>Forecast for {forecastDate[1]}</Card.Title>
+                        <Card.Text>
+                            Tomorrow will be {forecastDescription[1]} with a high of {forecastMaxTemp[1]} and a low of {forecastLowTemp[2]}.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={`src/assets/icons/${forecastIcon[2]}.png`} />
                     <Card.Body>
                     <Card.Title>Forecast for {forecastDate[2]}</Card.Title>
                         <Card.Text>
-                            Tomorrow will be {forecastDescription[2]} with a high of {forecastMaxTemp[2]} and a low of {forecastLowTemp[2]}.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                    <Card.Title>Forecast for {forecastDate[3]}</Card.Title>
-                        <Card.Text>
-                            On {forecastDate[3]} there will be {forecastDescription[3]} with a high of {forecastMaxTemp[3]} and a low of {forecastLowTemp[3]}.
+                            On {forecastDate[2]} there will be {forecastDescription[2]} with a high of {forecastMaxTemp[3]} and a low of {forecastLowTemp[3]}.
                         </Card.Text>
                     </Card.Body>
                 </Card>
